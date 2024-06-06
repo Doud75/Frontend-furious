50619f9e5d963034c73ef0acf5aa75bf6d860496
Doud75 <adrienquimbre@gmail.com>
2024-06-06 23:04:55 +0200

    joystick and webSocket

--- utils/joystick.tsx
+++ utils/joystick.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {View, PanResponder, StyleSheet} from 'react-native';
 
 // @ts-ignore
 const Joystick = ({radius, innerRadius}) => {
   const [position, setPosition] = useState({x: 0, y: 0});
+  const ws = useRef<WebSocket | null>(null);
 
   const effectiveRadius = radius - innerRadius;
 
-  const logPosition = (x: number, y: number) => {
-    console.log({x: x.toFixed(1), y: y.toFixed(1)});
+  const calculatePower = (x: number, y: number) => {
+    const maxPower = 4095;
+    const normX = -x / 20;
+    const normY = -y / 20;
+    const motor1Power = Math.round(maxPower * (normY - normX));
+    const motor2Power = Math.round(maxPower * (normY - normX));
+    const motor3Power = Math.round(maxPower * (normY + normX));
+    const motor4Power = Math.round(maxPower * (normY + normX));
+
+    return [motor1Power, motor2Power, motor3Power, motor4Power];
   };
 
   const panResponder = PanResponder.create({
@@ -17,28 +26,83 @@ const Joystick = ({radius, innerRadius}) => {
       const newX = position.x + gesture.dx;
       const newY = position.y + gesture.dy;
 
+      // Calculate distance from center
       const distance = Math.sqrt(Math.pow(newX, 2) + Math.pow(newY, 2));
 
+      // If the move is within the effective radius, update position
       if (distance < effectiveRadius) {
         setPosition({x: newX, y: newY});
-        logPosition(newX, newY);
+        const power = calculatePower(newX, newY);
+        // Envoyer les données via WebSocket
+        if (ws.current && ws.current?.readyState === WebSocket.OPEN) {
+          const message = {
+            cmd: 1,
+            data: power,
+          };
+          console.log(power);
+          ws.current.send(JSON.stringify(message));
+        }
       } else {
+        // Limit movement to stay within the effective radius
         const angle = Math.atan2(newY, newX);
         setPosition({
           x: Math.cos(angle) * effectiveRadius,
           y: Math.sin(angle) * effectiveRadius,
         });
-        logPosition(
+        const power = calculatePower(
           Math.cos(angle) * effectiveRadius,
           Math.sin(angle) * effectiveRadius,
         );
+        // Envoyer les données via WebSocket
+        if (ws.current && ws.current?.readyState === WebSocket.OPEN) {
+          const message = {
+            cmd: 1,
+            data: power,
+          };
+          console.log(power);
+          ws.current.send(JSON.stringify(message));
+        }
       }
     },
     onPanResponderRelease: () => {
+      // Reset position when the touch is released
       setPosition({x: 0, y: 0});
-      logPosition(0, 0);
+      const power = calculatePower(0, 0);
+      // Envoyer les données via WebSocket
+      if (ws.current && ws.current?.readyState === WebSocket.OPEN) {
+        const message = {
+          cmd: 1,
+          data: power,
+        };
+        console.log(power);
+        ws.current.send(JSON.stringify(message));
+      }
     },
   });
+
+  useEffect(() => {
+    ws.current = new WebSocket('ws://192.168.43.12/grp4');
+
+    ws.current.onopen = () => {
+      console.log('WebSocket connected');
+    };
+
+    ws.current.onmessage = e => {
+      console.log('Received message: ', e.data);
+    };
+
+    ws.current.onerror = e => {
+      console.log('WebSocket error: ', e.message);
+    };
+
+    ws.current.onclose = e => {
+      console.log('WebSocket closed: ', e.code, e.reason);
+    };
+
+    return () => {
+      ws.current?.close();
+    };
+  }, []);
 
   return (
     <View
