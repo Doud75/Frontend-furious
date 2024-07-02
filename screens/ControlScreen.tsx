a74288afc293e54a8b94115c1f66459533238145
Doud75 <adrienquimbre@gmail.com>
2024-07-02 14:02:38 +0200

    set camera

--- screens/ControlScreen.tsx
+++ screens/ControlScreen.tsx
@@ -1,169 +1,7 @@
-/*
-import {StyleSheet, View, Text, Pressable} from 'react-native';
-import {useEffect, useRef} from 'react';
-
-export default function () {
-  let inMotion: string = '';
-  const ws = useRef<WebSocket | null>(null);
-
-  useEffect(() => {
-    ws.current = new WebSocket('ws://192.168.43.12/grp4');
-
-    ws.current.onopen = () => {
-      console.log('WebSocket connected');
-    };
-
-    ws.current.onmessage = e => {
-      console.log('Received message: ', e.data);
-    };
-
-    ws.current.onerror = e => {
-      console.log('WebSocket error: ', e.message);
-    };
-
-    ws.current.onclose = e => {
-      console.log('WebSocket closed: ', e.code, e.reason);
-    };
-
-    return () => {
-      ws.current?.close();
-    };
-  }, []);
-
-  const handlePress = (data: Array<number>, motion: string) => {
-    inMotion = motion;
-    console.log(data);
-    if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-      const message = {
-        cmd: 1,
-        data: data,
-      };
-      ws.current.send(JSON.stringify(message));
-    }
-  };
-
-  const handlePressTurn = (direction: string) => {
-    console.log(inMotion);
-    const powerTurnSide: number = 2095;
-    let data: Array<number> = [];
-    if (inMotion === 'front') {
-      if (direction === 'right') {
-        data = [
-          powerTurnSide + 2000,
-          powerTurnSide + 2000,
-          powerTurnSide,
-          powerTurnSide,
-        ];
-      } else if (direction === 'left') {
-        data = [
-          powerTurnSide,
-          powerTurnSide,
-          powerTurnSide + 2000,
-          powerTurnSide + 2000,
-        ];
-      } else {
-        data = [
-          powerTurnSide + 2000,
-          powerTurnSide + 2000,
-          powerTurnSide + 2000,
-          powerTurnSide + 2000,
-        ];
-      }
-    } else if (inMotion === 'back') {
-      if (direction === 'right') {
-        data = [
-          -powerTurnSide + 2000,
-          -powerTurnSide + 2000,
-          -powerTurnSide,
-          -powerTurnSide,
-        ];
-      } else if (direction === 'left') {
-        data = [
-          -powerTurnSide,
-          -powerTurnSide,
-          -powerTurnSide + 2000,
-          -powerTurnSide + 2000,
-        ];
-      } else {
-        data = [
-          -powerTurnSide + 2000,
-          -powerTurnSide + 2000,
-          -powerTurnSide + 2000,
-          -powerTurnSide + 2000,
-        ];
-      }
-    } else {
-      if (direction === 'right') {
-        data = [powerTurnSide, powerTurnSide, -powerTurnSide, -powerTurnSide];
-      } else if (direction === 'left') {
-        data = [-powerTurnSide, -powerTurnSide, powerTurnSide, powerTurnSide];
-      } else {
-        data = [0, 0, 0, 0];
-      }
-    }
-    console.log(data);
-    if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-      const message = {
-        cmd: 1,
-        data: data,
-      };
-      ws.current.send(JSON.stringify(message));
-    }
-  };
-
-  return (
-    <View style={styles.conversationItem}>
-      <Pressable
-        style={styles.button}
-        onPressIn={() => handlePress([4095, 4095, 4095, 4095], 'front')}
-        onPressOut={() => handlePress([0, 0, 0, 0], '')}>
-        <Text style={styles.text}>Avancer</Text>
-      </Pressable>
-      <Pressable
-        style={styles.button}
-        onPressIn={() => handlePress([-4095, -4095, -4095, -4095], 'back')}
-        onPressOut={() => handlePress([0, 0, 0, 0], '')}>
-        <Text style={styles.text}>Reculer</Text>
-      </Pressable>
-      <Pressable
-        style={styles.button}
-        onPressIn={() => handlePressTurn('right')}
-        onPressOut={() => handlePressTurn('straight')}>
-        <Text style={styles.text}>Droite</Text>
-      </Pressable>
-      <Pressable
-        style={styles.button}
-        onPressIn={() => handlePressTurn('left')}
-        onPressOut={() => handlePressTurn('straight')}>
-        <Text style={styles.text}>Gauche</Text>
-      </Pressable>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  conversationItem: {
-    padding: 10,
-    backgroundColor: '#f9f9f9',
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-  button: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
-    marginTop: 10,
-  },
-  text: {
-    color: 'white',
-    textAlign: 'center',
-  },
-});
-*/
-
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import Joystick from '../utils/joystick.tsx';
+import Camera from '../utils/camera.tsx';
 import {WebSocketProvider} from '../context/WebSocketContext.tsx';
 
 const ControlScreen = () => {
@@ -172,16 +10,29 @@ const ControlScreen = () => {
       <View style={styles.container}>
         <Joystick radius={100} innerRadius={80} />
       </View>
+      <View style={styles.camera}>
+        <Camera />
+      </View>
     </WebSocketProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    zIndex: 2,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'white',
+    position: 'absolute',
+  },
+  camera: {
+    zIndex: 1,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    position: 'absolute',
   },
 });
 
---
+++ utils/camera.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Dimensions} from 'react-native';
+import WebView from 'react-native-webview';
+import {useWebSocket} from '../context/WebSocketContext.tsx';
+
+const {width, height} = Dimensions.get('window');
+
+const Camera: React.FC = () => {
+  const [ws] = useWebSocket();
+  if (ws && ws.readyState === WebSocket.OPEN) {
+    const message = {
+      cmd: 9,
+      data: 1,
+    };
+    ws.send(JSON.stringify(message));
+  }
+  return (
+    <WebView
+      scalesPageToFit={true}
+      bounces={false}
+      javaScriptEnabled
+      style={{height: width, width: height}}
+      source={{
+        html: `
+          <img style="display: block;-webkit-user-select: none;margin: auto;" src="http://192.168.43.12:7000/" width="100%" alt="">
+        `,
+      }}
+      automaticallyAdjustContentInsets={false}
+    />
+  );
+};
+export default Camera;
