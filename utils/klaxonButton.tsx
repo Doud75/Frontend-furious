295c2fad30d0902a978a9c5c374ee119d8af6667
Doud75 <adrienquimbre@gmail.com>
2024-07-03 09:40:57 +0200

    corection klaxon

--- utils/klaxonButton.tsx
+++ utils/klaxonButton.tsx
@@ -1,28 +1,28 @@
 import React, {useEffect, useRef} from 'react';
 import {TouchableOpacity, Text, StyleSheet, Alert} from 'react-native';
-import {useWebSocket} from "../context/WebSocketContext.tsx";
+import {useWebSocket} from '../context/WebSocketContext.tsx';
 
 const KlaxonButton = () => {
-  const ON = "on"
-  const OFF = "off"
+  const ON = 'on';
+  const OFF = 'off';
 
   const [ws] = useWebSocket();
-  let lastSentTime = 0;
 
-  const Klaxon = (active : string) => {
-
-    if (ws && ws.readyState === WebSocket.OPEN && Date.now() - lastSentTime > 1000) {
+  const Klaxon = (active: string) => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
       const message = {
         cmd: 7,
         data: active == ON ? 1 : 0,
-      }
+      };
       ws.send(JSON.stringify(message));
-      lastSentTime = Date.now();
     }
   };
 
   return (
-    <TouchableOpacity style={styles.button} onPressIn={() => Klaxon(ON)} onPressOut={() => Klaxon(OFF)}>
+    <TouchableOpacity
+      style={styles.button}
+      onPressIn={() => Klaxon(ON)}
+      onPressOut={() => Klaxon(OFF)}>
       <Text style={styles.buttonText}>Klaxon</Text>
     </TouchableOpacity>
   );
