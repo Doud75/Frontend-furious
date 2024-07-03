d75685a1c2e61447d31a497d9a805d9c7bfcab1c
Doud75 <adrienquimbre@gmail.com>
2024-07-03 15:49:20 +0200

    set socketIo

--- screens/ControlScreen.tsx
+++ screens/ControlScreen.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import {View, StyleSheet} from 'react-native';
 import Joystick from '../utils/joystick.tsx';
 import Camera from '../utils/camera.tsx';
 import {WebSocketProvider} from '../context/WebSocketContext.tsx';
-import Klaxon from "../utils/klaxonButton.tsx";
+import Klaxon from '../utils/klaxonButton.tsx';
+import io from 'socket.io-client';
 
 const ControlScreen = () => {
+
+  useEffect(() => {
+    console.log('start');
+    const socket = io('http://192.168.43.114:4500');
+    console.log('start');
+
+    socket.emit('joinGroup', 'idCourse');
+
+    socket.on('newMessage', message => {
+      console.log(message);
+      console.log('from Control Screen');
+    });
+    return () => {
+      console.log('return');
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <WebSocketProvider camera={true}>
       <View style={styles.container}>
--- screens/RacingScreen.tsx
+++ screens/RacingScreen.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import io from 'socket.io-client';
 
 const Racing = () => {
-    return(
-        <View style={styles.container}>
-            <Text>Racing</Text>
-        </View>
-    )
-}
+  useEffect(() => {
+    console.log('start');
+    const socket = io('http://192.168.43.114:4500');
+    console.log('start');
+
+    socket.emit('joinGroup', 'idCourse');
+
+    socket.on('newMessage', message => {
+      console.log(message);
+    });
+    return () => {
+      console.log('return');
+      socket.disconnect();
+    };
+  }, []);
+  return (
+    <View style={styles.container}>
+      <Text>Racing</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'white',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
 });
 
 export default Racing;
